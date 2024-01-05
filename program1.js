0053facd5afc0668593c1b91caa7147c3d1229b1
suraj9131 <155715354+suraj9131@users.noreply.github.com>
2024-01-05 13:02:14 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,11 +1,24 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
+var isValid = function (s) {
+
+    const left_Symbols = [];
     
+    for (let i = 0; i < s.length; i++) {
+        
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            left_Symbols.push(s[i]);
+        }
+       
+        else if (s[i] === ')' && left_Symbols.length !== 0 && left_Symbols[left_Symbols.length - 1] === '(') {
+            left_Symbols.pop();
+        } else if (s[i] === '}' && left_Symbols.length !== 0 && left_Symbols[left_Symbols.length - 1] === '{') {
+            left_Symbols.pop();
+        } else if (s[i] === ']' && left_Symbols.length !== 0 && left_Symbols[left_Symbols.length - 1] === '[') {
+            left_Symbols.pop();
+        }
+        
+        else {
+            return false;
+        }
+    }
+    return left_Symbols.length === 0;
 };
-
-module.exports = { isValid };
-
-
